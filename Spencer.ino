e79be28ec9312331841a6e1a778766710ba667a6
Filip Budiša <filip@circuitmess.com>
2020-08-31 13:39:24 +0200

    Add SerialFlash begin to setup

--- Spencer.ino
+++ Spencer.ino
@@ -1,7 +1,16 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <SerialFlash.h>
 
 void setup(){
+	Serial.begin(115200);
 
+	SPIClass spi(3);
+	spi.begin(18, 19, 23, 26);
+	if(!SerialFlash.begin(spi, 26)){
+		Serial.println("Flash fail");
+		return;
+	}
 }
 
 void loop(){
