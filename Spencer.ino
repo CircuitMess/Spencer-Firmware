d8979549240072fbe50fba0764ef62b7b67b933b
Emil Gajšak <emil.gajsak@gmail.com>
2020-09-08 19:02:29 +0200

    Working with in-lib changes of esp8266audio

--- Spencer.ino
+++ Spencer.ino
@@ -56,9 +56,16 @@ void setup(){
 	// 	Serial.print(buffer[i]);
 	// }
 	// Serial.println();
+	out = new AudioOutputI2S(0,0,16,-1);
+	out->SetRate(16000);
+	out->SetPinout(16, 21, 4);
+	out->SetChannels(1);
+	out->SetOutputModeMono(1);
+	out->SetGain(0.1);
+	// audio.begin();
 
 	input.setBtnPressCallback(17, [](){
-		audio.begin();
+		// audio.begin();
 		Serial.println("start recording!");
 		audio.record([](byte* data, size_t size){
 
@@ -98,54 +105,46 @@ void setup(){
 		// 	Serial.print(sample);
 		// 	Serial.printf(", %d, %d\n", -(std::numeric_limits<int32_t>::max()), std::numeric_limits<int32_t>::max());
 		// }
+		if(SerialFlash.exists("recording.wav"))
+		{
+			Serial.println("exists");
+		}
+		else
+		{
+			Serial.println("doesnt exist");
+		}
+		SerialFlashFile readfile = SerialFlash.open("recording.wav");
+		Serial.println(readfile.getFlashAddress());		
+		char buffer[10] = {0};
+		uint32_t readBytes = sizeof(buffer);
+		for(uint8_t i = 0; i < 5; i++)
+		{
+			readBytes = readfile.read(buffer, 10);
+			for(uint8_t i = 0; i < readBytes; i++)
+			{
+				Serial.print(buffer[i]);
+			}
+		}
+		Serial.println();
+		readfile.close();
 
+		file = new AudioFileSourceSerialFlash();
+		if(!file->open("recording.wav"))
+		{
+			Serial.println("error opening file");
+			// delay(10000);
+		}
+
+		// i2s_stop(I2S_NUM_0);
+		wav = new AudioGeneratorWAV();
+		// mp3 = new AudioGeneratorMP3();
+		
+		// mp3->begin(file, out);
+		wav->begin(file, out);
 		
 	});
 
-	if(SerialFlash.exists("recording.wav"))
-	{
-		Serial.println("exists");
-	}
-	else
-	{
-		Serial.println("doesnt exist");
-	}
-	SerialFlashFile readfile = SerialFlash.open("recording.wav");
-	Serial.println(readfile.getFlashAddress());		
-	char buffer[10] = {0};
-	uint32_t readBytes = sizeof(buffer);
-	for(uint8_t i = 0; i < 5; i++)
-	{
-		readBytes = readfile.read(buffer, 10);
-		for(uint8_t i = 0; i < readBytes; i++)
-		{
-			Serial.print(buffer[i]);
-		}
-	}
-	Serial.println();
-	readfile.close();
-
-	file = new AudioFileSourceSerialFlash();
-	if(!file->open("recording.wav"))
-	{
-		Serial.println("error opening file");
-		// delay(10000);
-	}
-
-	audio.begin();
-	// i2s_stop(I2S_NUM_0);
-	wav = new AudioGeneratorWAV();
-	// mp3 = new AudioGeneratorMP3();
-	out = new AudioOutputI2S(0,0,16,-1);
 	
-	out->SetRate(16000);
-	out->SetPinout(16, 21, 4);
-	out->SetBitsPerSample(16);
-	out->SetChannels(1);
-	out->SetOutputModeMono(1);
-	out->SetGain(0.5);
-	// mp3->begin(file, out);
-	wav->begin(file, out);
 }
 
 void loop(){
