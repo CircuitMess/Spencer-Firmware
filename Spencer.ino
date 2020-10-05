5191413e9fc79bb1e53f55de04c335d6e0856cd5
Emil Gajšak <emil.gajsak@gmail.com>
2020-10-05 18:11:31 +0200

    Led matrix (#2)
    * Added LEDmatrix
    
    * Added gifdec adapted to SerialFlash
    
    * Gif animation example
    
    * Added gif animation support on the led matrix
    
    * Added global color table check in gifdec
    
    * Added monochrome option in gif rendering

--- Spencer.ino
+++ Spencer.ino
@@ -6,8 +6,12 @@
 #include "src/Speech/SpeechToIntent.h"
 #include "src/Services/Audio/Audio.h"
 #include "Spencer.hpp"
+#include "src/LEDmatrix/LEDmatrix.h"
+#include <Loop/LoopManager.h>
+
 InputGPIO input;
 Audio audio;
+LEDmatrix ledmatrix;
 
 void setup(){
 	Serial.begin(115200);
@@ -18,6 +22,17 @@ void setup(){
 		Serial.println("Flash fail");
 		return;
 	}
+
+	if(!ledmatrix.begin())
+	{
+		Serial.println("couldn't start matrix");
+		while(1);
+	}
+	ledmatrix.clear();
+	ledmatrix.setBrightness(20);
+	ledmatrix.setRotation(2);
 }
 void loop(){
+
 }
+
