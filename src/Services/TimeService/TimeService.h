75c71e48911bb1a167e872e8a27d32be43ea50ed
Emil Gajšak <emil.gajsak@gmail.com>
2020-10-19 21:40:46 +0200

    Added timeService fetch

--- src/Services/TimeService/TimeService.h
+++ src/Services/TimeService/TimeService.h
@@ -2,15 +2,21 @@
 #define SPENCER_TIMESERVICE_H
 
 #include "DateTime.hpp"
+class WiFiUDP;
+class NTPClient;
 class TimeServiceImpl
 {
 public:
 	uint getTime();
 	void setTime(uint unixTime);
-
+	bool fetchTime();
 private:
 	uint unixtime = 0;
-	uint currentMillis = 0;
+	uint currentMillis = millis();
+	WiFiUDP *ntpUDP;
+	// By default 'pool.ntp.org' is used with 60 seconds update interval and
+	// no offset
+	NTPClient *timeClient;
 };
 extern TimeServiceImpl TimeService;
 
