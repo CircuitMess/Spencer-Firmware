8fd4584eec33a51301f87fd9289a32f032b25c04
Filip Budiša <filip@circuitmess.com>
2020-11-09 12:19:43 +0100

    Fix sentences script breaking on single quotes

--- scripts/sentences.sh
+++ scripts/sentences.sh
@@ -13,18 +13,18 @@ while IFS= read -r sentence; do
   echo "$dir/$name: $sentence"
   curl -s --location --request POST "https://texttospeech.googleapis.com/v1/text:synthesize?key=$KEY" \
     --header "Content-Type: application/json" \
-    --data-raw "{
-    'input': { 'text': '$sentence' },
-    'voice': {
-        'languageCode': 'en-US',
-        'name': 'en-US-Standard-D',
-        'ssmlGender': 'NEUTRAL'
+    --data-raw '{
+    "input": { "text": "'"$sentence"'" },
+    "voice": {
+        "languageCode": "en-US",
+        "name": "en-US-Standard-D",
+        "ssmlGender": "NEUTRAL"
     },
-    'audioConfig': {
-        'audioEncoding': 'MP3',
-        'speakingRate': 0.96,
-        'pitch': 7.2,
-        'sampleRateHertz': 16000
+    "audioConfig": {
+        "audioEncoding": "MP3",
+        "speakingRate": 0.96,
+        "pitch": 7.2,
+        "sampleRateHertz": 16000
     }
-  }" | jq -r '.audioContent' | base64 -d > "data/$dir/$name.mp3"
+  }' | jq -r '.audioContent' | base64 -d > "data/$dir/$name.mp3"
 done < sentences.txt
